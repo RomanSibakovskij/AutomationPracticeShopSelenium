9b7f27a10baf1d7334bb5abf0a3286fdcfddcfa6
RomanSibakovskij <j4023958@gmail.com>
2025-02-01 06:11:37 +0200

    Add empty HomePage sheet

---
+++ src/main/java/com/nopcommerce/demo/HomePage.java
@@ -0,0 +1,15 @@
+package com.nopcommerce.demo;
+
+import com.nopcommerce.demo.utilities.BasePage;
+import org.openqa.selenium.*;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.*;
+import org.openqa.selenium.support.ui.*;
+
+import java.time.Duration;
+
+public class HomePage extends BasePage{
+
+    public HomePage(WebDriver driver) {super(driver);}
+    
+}
