081d2e4cc46c71d5b78343c86d22842632edd0b4
RomanSibakovskij <j4023958@gmail.com>
2025-02-01 06:24:18 +0200

    Add empty CreateAccountPage sheet

---
+++ src/main/java/com/nopcommerce/demo/CreateAccountPage.java
@@ -0,0 +1,17 @@
+package com.nopcommerce.demo;
+
+import com.nopcommerce.demo.utilities.BasePage;
+import org.openqa.selenium.*;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.*;
+import org.openqa.selenium.support.ui.*;
+
+import java.time.Duration;
+
+public class CreateAccountPage extends BasePage{
+
+
+    public CreateAccountPage(WebDriver driver) {super(driver);}
+
+    
+}
