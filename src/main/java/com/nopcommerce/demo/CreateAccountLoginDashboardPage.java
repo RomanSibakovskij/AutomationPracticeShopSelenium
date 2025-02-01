60d3e17c08001c84c22d6cf66bfc9f4f274d1ddb
RomanSibakovskij <j4023958@gmail.com>
2025-02-01 06:19:29 +0200

    Add empty CreateAccountLoginDashboardPage sheet

---
+++ src/main/java/com/nopcommerce/demo/CreateAccountLoginDashboardPage.java
@@ -0,0 +1,17 @@
+package com.nopcommerce.demo;
+
+import com.nopcommerce.demo.utilities.BasePage;
+import org.openqa.selenium.*;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.*;
+import org.openqa.selenium.support.ui.*;
+
+import java.time.Duration;
+
+public class CreateAccountLoginDashboardPage extends BasePage {
+
+    
+    public CreateAccountLoginDashboardPage(WebDriver driver) {super(driver);}
+
+
+}
