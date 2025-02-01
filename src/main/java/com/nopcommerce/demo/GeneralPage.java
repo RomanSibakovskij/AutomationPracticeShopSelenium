72830208f43224410ccbff492da6bb7892dcea00
RomanSibakovskij <j4023958@gmail.com>
2025-02-01 06:09:23 +0200

    Add web elements for GeneralPage

--- src/main/java/com/nopcommerce/demo/GeneralPage.java
+++ src/main/java/com/nopcommerce/demo/GeneralPage.java
@@ -10,8 +10,276 @@ import java.time.Duration;
 
 public class GeneralPage extends BasePage {
 
+    //general page web elements
+    //header
+    @FindBy(xpath = "//div[@class='banner']//a")
+    private WebElement headerBannerLink;
+    @FindBy(xpath = "//div[@class='nav']//span[@class='shop-phone']")
+    private WebElement headerShopPhone;
+    @FindBy(xpath = "//div[@class='nav']//a[@class='login']")
+    private WebElement loginNavLink;
+    @FindBy(xpath = "//div[@class='nav']//a[@title='Contact us']")
+    private WebElement contactUsNavLink;
+    @FindBy(xpath = "//div[@id='header_logo']/a")
+    private WebElement headerLogoLink;
+    @FindBy(xpath = "//div[@id='search_block_top']//input[@id='search_query_top']")
+    private WebElement headerSearchBarInputField;
+    @FindBy(xpath = "//div[@id='search_block_top']//button")
+    private WebElement searchButton;
+    @FindBy(xpath = "//div[@class='shopping_cart']/a")
+    private WebElement shoppingCartLinkButton;
+    //header lower nav bar
+    @FindBy(xpath = "//div[@id='block_top_menu']/ul/li[1]/a")
+    private WebElement womenNavLink;
+    @FindBy(xpath = "//div[@id='block_top_menu']/ul/li[3]/a")
+    private WebElement tShirtsNavLink;
+    @FindBy(xpath = "//div[@id='block_top_menu']/ul/li[4]/a")
+    private WebElement blogNavLink;
+    //footer
+    @FindBy(xpath = "//div[@id='newsletter_block_left']/h4")
+    private WebElement footerNewsletterSectionTitle;
+    @FindBy(xpath = "//div[@id='newsletter_block_left']//input[@id='newsletter-input']")
+    private WebElement footerNewsletterInputField;
+    @FindBy(xpath = "//div[@id='newsletter_block_left']//input[@id='newsletter-input']")
+    private WebElement footerNewsletterSubmitButton;
+    @FindBy(xpath = "//section[@id='social_block']//h4")
+    private WebElement footerFollowUsSectionTitle;
+    @FindBy(xpath = "//section[@id='social_block']/ul/li[1]")
+    private WebElement footerFollowUsFacebookLogoLink;
+    @FindBy(xpath = "//section[@id='social_block']/ul/li[2]")
+    private WebElement footerFollowUsXLogoLink;
+    @FindBy(xpath = "//section[@id='social_block']/ul/li[3]")
+    private WebElement footerFollowUsRSSLogoLink;
+    @FindBy(xpath = "//section[@class='blockcategories_footer footer-block col-xs-12 col-sm-2']/h4")
+    private WebElement footerCategoriesSectionTitle;
+    @FindBy(xpath = "//section[@class='blockcategories_footer footer-block col-xs-12 col-sm-2']//ul[@class='tree dynamized']/li/a")
+    private WebElement footerWomenLink;
+    @FindBy(xpath = "//section[@class='footer-block col-xs-12 col-sm-2']/h4")
+    private WebElement footerInformationSectionTitle;
+    @FindBy(xpath = "//section[@class='footer-block col-xs-12 col-sm-2']/ul/li[1]/a")
+    private WebElement footerInformationStoresLink;
+    @FindBy(xpath = "//section[@class='footer-block col-xs-12 col-sm-2']/ul/li[2]/a")
+    private WebElement footerInformationTermsOfUseLink;
+    @FindBy(xpath = "//section[@class='footer-block col-xs-12 col-sm-2']/ul/li[3]/a")
+    private WebElement footerInformationAboutUsLink;
+    @FindBy(xpath = "//section[@class='footer-block col-xs-12 col-sm-4']/h4")
+    private WebElement footerMyAccountSectionTitle;
+    @FindBy(xpath = "//section[@class='footer-block col-xs-12 col-sm-4']//ul[@class='bullet']/li[1]/a")
+    private WebElement footerOrdersLink;
+    @FindBy(xpath = "//section[@class='footer-block col-xs-12 col-sm-4']//ul[@class='bullet']/li[2]/a")
+    private WebElement footerCreditSlipsLink;
+    @FindBy(xpath = "//section[@class='footer-block col-xs-12 col-sm-4']//ul[@class='bullet']/li[3]/a")
+    private WebElement footerAddressesLink;
+    @FindBy(xpath = "//section[@class='footer-block col-xs-12 col-sm-4']//ul[@class='bullet']/li[4]/a")
+    private WebElement footerMyPersonalInfoLink;
+    @FindBy(xpath = "//section[@id='block_contact_infos']//h4")
+    private WebElement footerStoreInformationSectionTitle;
+    @FindBy(xpath = "//section[@id='block_contact_infos']//ul/li[1]")
+    private WebElement footerStoreAddress;
+    @FindBy(xpath = "//section[@id='block_contact_infos']//ul/li[2]")
+    private WebElement footerCallStorePhoneText;
+    @FindBy(xpath = "//section[@id='block_contact_infos']//ul/li[3]")
+    private WebElement footerEmailText;
+    @FindBy(xpath = "//section[@id='block_contact_infos']//ul/li[3]//a")
+    private WebElement footerEmailLink;
+    @FindBy(xpath = "//section[@class='bottom-footer col-xs-12']//a")
+    private WebElement footerCopyrightTextLink;
 
     public GeneralPage(WebDriver driver) {super(driver);}
 
+    //footer information section title getter
+    public String getFooterInfoSectionTitle(){return footerInformationSectionTitle.getText();}
+    //footer categories section title getter
+    public String getFooterCategoriesSectionTitle(){return footerCategoriesSectionTitle.getText();}
+    //footer my account section title getter
+    public String getFooterMyAccountSectionTitle(){return footerMyAccountSectionTitle.getText();}
+    //footer follow us section title getter
+    public String getFooterFollowUsSectionTitle(){return footerFollowUsSectionTitle.getText();}
+    //footer newsletter section title getter
+    public String getFooterNewsletterSectionTitle(){return footerNewsletterSectionTitle.getText();}
+    //footer store information section title getter
+    public String getStoreInfoSectionTitle(){return footerStoreInformationSectionTitle.getText();}
+    //footer store information address getter
+    public String getStoreInfoAddress(){return footerStoreAddress.getText();}
+    //footer store information phone text getter
+    public String getStoreInfoPhoneText(){return footerCallStorePhoneText.getText();}
+    //footer email link text getter
+    public String getFooterEmailLink(){return footerEmailLink.getText();}
+    //footer copyright text getter
+    public String getFooterCopyrightTextLink(){return footerCopyrightTextLink.getText();}
+
+    //general page web element assert methods (elements all pages share)
+    //header elements
+    public boolean isHeaderBannerLinkDisplayed() {
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofMillis(550));
+        wait.until(ExpectedConditions.visibilityOf(headerBannerLink));
+        return headerBannerLink.isDisplayed();
+    }
+    public boolean isHeaderShopPhoneDisplayed() {
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofMillis(550));
+        wait.until(ExpectedConditions.visibilityOf(headerShopPhone));
+        return headerShopPhone.isDisplayed();
+    }
+    public boolean isHeaderContactUsNavLinkDisplayed() {
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofMillis(550));
+        wait.until(ExpectedConditions.visibilityOf(contactUsNavLink));
+        return contactUsNavLink.isDisplayed();
+    }
+    public boolean isHeaderLogoLinkDisplayed() {
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofMillis(550));
+        wait.until(ExpectedConditions.visibilityOf(headerLogoLink));
+        return headerLogoLink.isDisplayed();
+    }
+    public boolean isHeaderSearchBarInputFieldDisplayed() {
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofMillis(550));
+        wait.until(ExpectedConditions.visibilityOf(headerSearchBarInputField));
+        return headerSearchBarInputField.isDisplayed();
+    }
+    public boolean isSearchButtonDisplayed() {
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofMillis(550));
+        wait.until(ExpectedConditions.visibilityOf(searchButton));
+        return searchButton.isDisplayed();
+    }
+    public boolean isShoppingCartLinkButtonDisplayed() {
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofMillis(550));
+        wait.until(ExpectedConditions.visibilityOf(shoppingCartLinkButton));
+        return shoppingCartLinkButton.isDisplayed();
+    }
+    public boolean isWomenNavLinkDisplayed() {
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofMillis(550));
+        wait.until(ExpectedConditions.visibilityOf(womenNavLink));
+        return womenNavLink.isDisplayed();
+    }
+    public boolean isTShirtsNavLinkDisplayed() {
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofMillis(550));
+        wait.until(ExpectedConditions.visibilityOf(tShirtsNavLink));
+        return tShirtsNavLink.isDisplayed();
+    }
+    public boolean isBlogNavLinkDisplayed() {
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofMillis(550));
+        wait.until(ExpectedConditions.visibilityOf(blogNavLink));
+        return blogNavLink.isDisplayed();
+    }
+    //footer elements
+    public boolean isFooterNewsletterSubmitButtonDisplayed() {
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofMillis(550));
+        wait.until(ExpectedConditions.visibilityOf(footerNewsletterSubmitButton));
+        return footerNewsletterSubmitButton.isDisplayed();
+    }
+    public boolean isFooterInfoStoresLinkDisplayed() {
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofMillis(550));
+        wait.until(ExpectedConditions.visibilityOf(footerInformationStoresLink));
+        return footerInformationStoresLink.isDisplayed();
+    }
+    public boolean isFooterInfoTermsOfUseLinkDisplayed() {
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofMillis(550));
+        wait.until(ExpectedConditions.visibilityOf(footerInformationTermsOfUseLink));
+        return footerInformationTermsOfUseLink.isDisplayed();
+    }
+    public boolean isFooterInfoAboutUsLinkDisplayed() {
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofMillis(550));
+        wait.until(ExpectedConditions.visibilityOf(footerInformationAboutUsLink));
+        return footerInformationAboutUsLink.isDisplayed();
+    }
+    public boolean isFooterCategoriesSectionTitleDisplayed() {
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofMillis(550));
+        wait.until(ExpectedConditions.visibilityOf(footerCategoriesSectionTitle));
+        return footerCategoriesSectionTitle.isDisplayed();
+    }
+    public boolean isFooterWomenLinkDisplayed() {
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofMillis(550));
+        wait.until(ExpectedConditions.visibilityOf(footerWomenLink));
+        return footerWomenLink.isDisplayed();
+    }
+    public boolean isFooterMyAccountSectionTitleDisplayed() {
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofMillis(550));
+        wait.until(ExpectedConditions.visibilityOf(footerMyAccountSectionTitle));
+        return footerMyAccountSectionTitle.isDisplayed();
+    }
+    public boolean isFooterOrdersLinkDisplayed() {
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofMillis(550));
+        wait.until(ExpectedConditions.visibilityOf(footerOrdersLink));
+        return footerOrdersLink.isDisplayed();
+    }
+    public boolean isFooterCreditSlipsLinkDisplayed() {
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofMillis(550));
+        wait.until(ExpectedConditions.visibilityOf(footerCreditSlipsLink));
+        return footerCreditSlipsLink.isDisplayed();
+    }
+    public boolean isFooterMyAccountAddressesLinkDisplayed() {
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofMillis(550));
+        wait.until(ExpectedConditions.visibilityOf(footerAddressesLink));
+        return footerAddressesLink.isDisplayed();
+    }
+    public boolean isFooterMyPersonalInfoLinkDisplayed() {
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofMillis(550));
+        wait.until(ExpectedConditions.visibilityOf(footerMyPersonalInfoLink));
+        return footerMyPersonalInfoLink.isDisplayed();
+    }
+    public boolean isFooterStoreInfoSectionTitleDisplayed() {
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofMillis(550));
+        wait.until(ExpectedConditions.visibilityOf(footerStoreInformationSectionTitle));
+        return footerStoreInformationSectionTitle.isDisplayed();
+    }
+    public boolean isFooterCallStorePhoneTextDisplayed() {
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofMillis(550));
+        wait.until(ExpectedConditions.visibilityOf(footerCallStorePhoneText));
+        return footerCallStorePhoneText.isDisplayed();
+    }
+    public boolean isFooterFollowUsSectionTitleDisplayed() {
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofMillis(550));
+        wait.until(ExpectedConditions.visibilityOf(footerFollowUsSectionTitle));
+        return footerFollowUsSectionTitle.isDisplayed();
+    }
+    public boolean isFooterFollowUsFacebookLogoLinkDisplayed() {
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofMillis(550));
+        wait.until(ExpectedConditions.visibilityOf(footerFollowUsFacebookLogoLink));
+        return footerFollowUsFacebookLogoLink.isDisplayed();
+    }
+    public boolean isFooterFollowUsXLogoLinkDisplayed() {
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofMillis(550));
+        wait.until(ExpectedConditions.visibilityOf(footerFollowUsXLogoLink));
+        return footerFollowUsXLogoLink.isDisplayed();
+    }
+    public boolean isFooterFollowUsRSSLogoLinkDisplayed() {
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofMillis(550));
+        wait.until(ExpectedConditions.visibilityOf(footerFollowUsRSSLogoLink));
+        return footerFollowUsRSSLogoLink.isDisplayed();
+    }
+    public boolean isFooterNewsletterSectionTitleDisplayed() {
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofMillis(550));
+        wait.until(ExpectedConditions.visibilityOf(footerNewsletterSectionTitle));
+        return footerNewsletterSectionTitle.isDisplayed();
+    }
+    public boolean isFooterNewsletterInputFieldDisplayed() {
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofMillis(550));
+        wait.until(ExpectedConditions.visibilityOf(footerNewsletterInputField));
+        return footerNewsletterInputField.isDisplayed();
+    }
+    public boolean isFooterStoreInformationTitleDisplayed() {
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofMillis(550));
+        wait.until(ExpectedConditions.visibilityOf(footerStoreInformationSectionTitle));
+        return footerStoreInformationSectionTitle.isDisplayed();
+    }
+    public boolean isFooterStoreAddressDisplayed() {
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofMillis(550));
+        wait.until(ExpectedConditions.visibilityOf(footerStoreAddress));
+        return footerStoreAddress.isDisplayed();
+    }
+    public boolean isFooterEmailTextDisplayed() {
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofMillis(550));
+        wait.until(ExpectedConditions.visibilityOf(footerEmailText));
+        return footerEmailText.isDisplayed();
+    }
+    public boolean isFooterEmailLinkDisplayed() {
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofMillis(550));
+        wait.until(ExpectedConditions.visibilityOf(footerEmailLink));
+        return footerEmailLink.isDisplayed();
+    }
+    public boolean isFooterCopyrightTextLinkDisplayed() {
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofMillis(550));
+        wait.until(ExpectedConditions.visibilityOf(footerCopyrightTextLink));
+        return footerCopyrightTextLink.isDisplayed();
+    }
 
 }
--- src/test/java/com/nopcommerce/demo/utilities/TestMethods.java
+++ src/test/java/com/nopcommerce/demo/utilities/TestMethods.java
@@ -11,8 +11,82 @@ public class TestMethods extends BaseTest {
 
     protected static final Logger logger = LoggerFactory.getLogger(TestMethods.class);
 
+    ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+    //general page web element assert test method
+    protected void isGeneralPageWebElementDisplayed(GeneralPage generalPage){
+        //assert header banner link is displayed
+        assertTrue(generalPage.isHeaderBannerLinkDisplayed(), "The header banner link isn't displayed");
+        //assert header shop phone is displayed
+        assertTrue(generalPage.isHeaderShopPhoneDisplayed(), "The header shop phone isn't displayed");
+        //assert header contact us is displayed
+        assertTrue(generalPage.isHeaderContactUsNavLinkDisplayed(), "The header contact us navbar link isn't displayed");
+        //assert header logo link is displayed
+        assertTrue(generalPage.isHeaderLogoLinkDisplayed(), "The header logo link isn't displayed");
+        //assert header search bar input field is displayed
+        assertTrue(generalPage.isHeaderSearchBarInputFieldDisplayed(), "The header search bar input field isn't displayed");
+        //assert search button is displayed
+        assertTrue(generalPage.isSearchButtonDisplayed(), "The search button isn't displayed");
+        //assert shopping cart link button is displayed
+        assertTrue(generalPage.isShoppingCartLinkButtonDisplayed(), "The shopping cart link button isn't displayed");
+        //assert women nav link is displayed
+        assertTrue(generalPage.isWomenNavLinkDisplayed(), "The women nav link isn't displayed");
+        //assert t-shirts nav link is displayed
+        assertTrue(generalPage.isTShirtsNavLinkDisplayed(), "The t-shirts nav link isn't displayed");
+        //assert blog nav link is displayed
+        assertTrue(generalPage.isBlogNavLinkDisplayed(), "The blog nav link isn't displayed");
+        //assert footer newsletter submit button is displayed
+        assertTrue(generalPage.isFooterNewsletterSubmitButtonDisplayed(), "The footer newsletter submit button isn't displayed");
+        //assert footer information stores link is displayed
+        assertTrue(generalPage.isFooterInfoStoresLinkDisplayed(), "The footer information stores link isn't displayed");
+        //assert footer information terms of use link is displayed
+        assertTrue(generalPage.isFooterInfoTermsOfUseLinkDisplayed(), "The footer information terms of use link isn't displayed");
+        //assert footer information about us link is displayed
+        assertTrue(generalPage.isFooterInfoAboutUsLinkDisplayed(), "The footer information about us link isn't displayed");
+        //assert footer categories section title is displayed
+        assertTrue(generalPage.isFooterCategoriesSectionTitleDisplayed(), "The footer categories section title isn't displayed");
+        //assert footer women link is displayed
+        assertTrue(generalPage.isFooterWomenLinkDisplayed(), "The footer women link isn't displayed");
+        //assert footer my account section title is displayed
+        assertTrue(generalPage.isFooterMyAccountSectionTitleDisplayed(), "The footer my account section title isn't displayed");
+        //assert footer orders link is displayed
+        assertTrue(generalPage.isFooterOrdersLinkDisplayed(), "The footer orders link isn't displayed");
+        //assert footer credit slips link is displayed
+        assertTrue(generalPage.isFooterCreditSlipsLinkDisplayed(), "The footer my account credit slips link isn't displayed");
+        //assert footer my account addresses link is displayed
+        assertTrue(generalPage.isFooterMyAccountAddressesLinkDisplayed(), "The footer my account addresses link isn't displayed");
+        //assert footer my personal info link is displayed
+        assertTrue(generalPage.isFooterMyPersonalInfoLinkDisplayed(), "The footer my personal info link isn't displayed");
+        //assert footer store information section title is displayed
+        assertTrue(generalPage.isFooterStoreInfoSectionTitleDisplayed(), "The footer store information section title isn't displayed");
+        //assert footer call store phone text is displayed
+        assertTrue(generalPage.isFooterCallStorePhoneTextDisplayed(), "The footer call store phone text isn't displayed");
+        //assert footer store address is displayed
+        assertTrue(generalPage.isFooterStoreAddressDisplayed(), "The footer call address text isn't displayed");
+        //assert footer follow us section title is displayed
+        assertTrue(generalPage.isFooterFollowUsSectionTitleDisplayed(), "The footer follow us section title isn't displayed");
+        //assert footer follow us facebook logo link is displayed
+        assertTrue(generalPage.isFooterFollowUsFacebookLogoLinkDisplayed(), "The footer follow us facebook logo link isn't displayed");
+        //assert footer follow us X logo link is displayed
+        assertTrue(generalPage.isFooterFollowUsXLogoLinkDisplayed(), "The footer follow us X logo link isn't displayed");
+        //assert footer follow us RSS logo link is displayed
+        assertTrue(generalPage.isFooterFollowUsRSSLogoLinkDisplayed(), "The footer follow us RSS logo link isn't displayed");
+        //assert footer newsletter section title is displayed
+        assertTrue(generalPage.isFooterNewsletterSectionTitleDisplayed(), "The footer newsletter section title isn't displayed");
+        //assert footer newsletter input field is displayed
+        assertTrue(generalPage.isFooterNewsletterInputFieldDisplayed(), "The footer newsletter input field isn't displayed");
+        //assert footer store information section title is displayed
+        assertTrue(generalPage.isFooterStoreInformationTitleDisplayed(), "The footer store information section title isn't displayed");
+        //assert footer email text is displayed
+        assertTrue(generalPage.isFooterEmailTextDisplayed(), "The footer email text isn't displayed");
+        //assert footer email link is displayed
+        assertTrue(generalPage.isFooterEmailLinkDisplayed(), "The footer email link isn't displayed");
+        //assert footer copyright text link is displayed
+        assertTrue(generalPage.isFooterCopyrightTextLinkDisplayed(), "The footer copyright text link isn't displayed");
+    }
 
 
+    ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
     ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
