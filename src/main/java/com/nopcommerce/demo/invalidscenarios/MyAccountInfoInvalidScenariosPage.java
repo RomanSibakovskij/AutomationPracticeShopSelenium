db2ae584037108c23efa891bc4de0fec3d587697
RomanSibakovskij <j4023958@gmail.com>
2025-02-02 16:19:50 +0200

    Add relevant web elements for MyAccountInfoInvalidScenariosPage

--- src/main/java/com/nopcommerce/demo/invalidscenarios/MyAccountInfoInvalidScenariosPage.java
+++ src/main/java/com/nopcommerce/demo/invalidscenarios/MyAccountInfoInvalidScenariosPage.java
@@ -9,6 +9,19 @@ import org.openqa.selenium.support.ui.*;
 
 public class MyAccountInfoInvalidScenariosPage extends BasePage {
 
+    @FindBy(xpath = "//div[@id='center_column']//input[@id='firstname']")
+    private WebElement myAccountInfoFirstNameInputField;
+    @FindBy(xpath = "//div[@id='center_column']//input[@id='lastname']")
+    private WebElement myAccountInfoLastNameInputField;
+    @FindBy(xpath = "//div[@id='center_column']//input[@id='email']")
+    private WebElement myAccountInfoEmailInputField;
+    @FindBy(xpath = "//div[@id='center_column']//input[@id='old_passwd']")
+    private WebElement myAccountInfoCurrentPasswordInputField;
+    @FindBy(xpath = "//div[@id='center_column']//input[@id='passwd']")
+    private WebElement myAccountInfoNewPasswordInputField;
+    @FindBy(xpath = "//div[@id='center_column']//input[@id='confirmation']")
+    private WebElement myAccountInfoConfirmPasswordInputField;
+
     public MyAccountInfoInvalidScenariosPage(WebDriver driver) {super(driver);}
 
 
