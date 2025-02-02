88817dff173d3f45ea09590f0a7ac728d8173a21
RomanSibakovskij <j4023958@gmail.com>
2025-02-02 08:11:48 +0200

    Add empty MyAccountInformationPage sheet

---
+++ src/main/java/com/nopcommerce/demo/MyAccountInformationPage.java
@@ -0,0 +1,17 @@
+package com.nopcommerce.demo;
+
+import com.nopcommerce.demo.utilities.*;
+import org.openqa.selenium.*;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.*;
+import org.openqa.selenium.support.ui.*;
+
+import java.time.Duration;
+
+public class MyAccountInformationPage extends BasePage {
+
+    
+    public MyAccountInformationPage(WebDriver driver) {super(driver);}
+
+
+}
