6475d7250b53787e97556c8d0d2cb6429aac124b
RomanSibakovskij <j4023958@gmail.com>
2025-02-02 16:17:51 +0200

    Add empty MyAccountInfoInvalidScenariosPage utility sheet

---
+++ src/main/java/com/nopcommerce/demo/invalidscenarios/MyAccountInfoInvalidScenariosPage.java
@@ -0,0 +1,15 @@
+package com.nopcommerce.demo.invalidscenarios;
+
+import com.nopcommerce.demo.utilities.*;
+import org.openqa.selenium.*;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.*;
+import org.openqa.selenium.support.ui.*;
+
+
+public class MyAccountInfoInvalidScenariosPage extends BasePage {
+
+    public MyAccountInfoInvalidScenariosPage(WebDriver driver) {super(driver);}
+
+
+}
