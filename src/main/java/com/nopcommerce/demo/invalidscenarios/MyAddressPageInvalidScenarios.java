447d2487d502d2db073573a8731bff629e60a54f
RomanSibakovskij <j4023958@gmail.com>
2025-02-04 05:59:56 +0200

    Add empty MyAddressPageInvalidScenarios utility sheet

---
+++ src/main/java/com/nopcommerce/demo/invalidscenarios/MyAddressPageInvalidScenarios.java
@@ -0,0 +1,17 @@
+package com.nopcommerce.demo.invalidscenarios;
+
+import com.nopcommerce.demo.utilities.*;
+import org.openqa.selenium.*;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.*;
+import org.openqa.selenium.support.ui.*;
+
+import java.time.Duration;
+
+public class MyAddressPageInvalidScenarios extends BasePage{
+
+
+    public MyAddressPageInvalidScenarios(WebDriver driver) {super(driver);}
+
+    
+}
