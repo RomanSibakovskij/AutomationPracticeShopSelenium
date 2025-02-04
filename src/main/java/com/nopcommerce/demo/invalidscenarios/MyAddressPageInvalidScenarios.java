9912b1c22da98b0a7dc7f9fb0bd17268d57e8404
RomanSibakovskij <j4023958@gmail.com>
2025-02-04 06:03:04 +0200

    Add relevant web elements for MyAddressPageInvalidScenarios

--- src/main/java/com/nopcommerce/demo/invalidscenarios/MyAddressPageInvalidScenarios.java
+++ src/main/java/com/nopcommerce/demo/invalidscenarios/MyAddressPageInvalidScenarios.java
@@ -10,8 +10,30 @@ import java.time.Duration;
 
 public class MyAddressPageInvalidScenarios extends BasePage{
 
+    @FindBy(xpath = "//div[@class='box']//input[@id='address1']")
+    private WebElement myAddress1InputField;
+    @FindBy(xpath = "//div[@class='box']//input[@id='address2']")
+    private WebElement myAddress2InputField;
+    @FindBy(xpath = "//div[@class='box']//input[@id='city']")
+    private WebElement myAddressCityInputField;
+    @FindBy(xpath = "//div[@class='box']//select[@id='id_state']")
+    private WebElement myAddressStateDropdownMenu;
+    @FindBy(xpath = "//select[@id='id_state']/option[@value='13']")
+    private WebElement myAddressStateIllinoisOption;
+    @FindBy(xpath = "//select[@id='id_state']/option[@value='1']")
+    private WebElement myAddressStateAlabamaOption;
+    @FindBy(xpath = "//div[@class='box']//input[@id='postcode']")
+    private WebElement myAddressPostalCodeInputField;
+    @FindBy(xpath = "//div[@class='box']//select[@id='id_country']")
+    private WebElement myAddressCountryDropdownMenu;
+    @FindBy(xpath = "//div[@class='box']//input[@id='phone']")
+    private WebElement myAddressHomePhoneInputField;
+    @FindBy(xpath = "//div[@class='box']//input[@id='phone_mobile']")
+    private WebElement myAddressMobilePhoneInputField;
+    @FindBy(xpath = "//div[@class='box']//input[@id='alias']")
+    private WebElement myAddressAliasInputField;
 
     public MyAddressPageInvalidScenarios(WebDriver driver) {super(driver);}
 
-    
+
 }
