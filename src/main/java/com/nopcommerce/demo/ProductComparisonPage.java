4bd189a0c15964619f6b677419e50742894ba349
RomanSibakovskij <j4023958@gmail.com>
2025-02-05 08:26:56 +0200

    Add empty ProductComparisonPage sheet

---
+++ src/main/java/com/nopcommerce/demo/ProductComparisonPage.java
@@ -0,0 +1,16 @@
+package com.nopcommerce.demo;
+
+import com.nopcommerce.demo.utilities.BasePage;
+import org.openqa.selenium.*;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.*;
+import org.openqa.selenium.support.ui.*;
+
+import java.time.Duration;
+import java.util.*;
+
+public class ProductComparisonPage extends BasePage{
+
+    public ProductComparisonPage(WebDriver driver) {super(driver);}
+    
+}
