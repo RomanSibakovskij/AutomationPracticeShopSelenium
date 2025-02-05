2e5e61355116f1fe9adde7b483c0856753b8f0b2
RomanSibakovskij <j4023958@gmail.com>
2025-02-05 17:52:39 +0200

    Add Remove 'Blouse' Product From Compare List Test (as a registered user) method

--- src/test/java/com/nopcommerce/demo/SingleProductCategoryDashboardPageTest.java
+++ src/test/java/com/nopcommerce/demo/SingleProductCategoryDashboardPageTest.java
@@ -45,7 +45,7 @@ public class SingleProductCategoryDashboardPageTest extends TestMethods{
         addBlouseAndPrintedDressToCompareListTest();
     }
 
-    //Test 008a -> navigate registered user to 'Women' product category page test (as a registered user)
+    //Test 008a -> add 'Blouse' and 'Printed Chiffon Dress' products to compare list test (as a registered user)
     @Test
     @DisplayName("Add 'Blouse' and 'Printed Chiffon Dress' Products To Compare List Test (as a registered user)")
     @Tag("Add_Products_To_Compare_List")
@@ -71,12 +71,35 @@ public class SingleProductCategoryDashboardPageTest extends TestMethods{
     @DisplayName("Remove 'Blouse' Product From Compare List Test (as a guest)")
     @Tag("Remove_Product_From_Compare_List")
     @Tag("Test_As_A_Guest")
-    void guestRemoveProductsFromCompareListTest(){
+    void guestRemoveProductFromCompareListTest(){
         //navigate to 'Women' category dashboard page test (as a guest)
         navigateToWomenCategoryProductDashboardPageTest();
         //add 'Blouse' and 'Printed Chiffon Dress' products to compare list test (as a guest)
         addBlouseAndPrintedDressToCompareListTest();
         //remove 'Blouse' product to compare list test (as a guest)
+        removeBlouseFromCompareListTest();
+    }
+
+    //Test 008c -> remove 'Blouse' product from compare list test (as a registered user)
+    @Test
+    @DisplayName("Remove 'Blouse' Product From Compare List Test (as a registered user)")
+    @Tag("Remove_Product_From_Compare_List")
+    @Tag("Test_As_A_Registered_User")
+    void regUserRemoveProductFromCompareListTest(){
+        CreateAccountPage createAccountPage = new CreateAccountPage(driver);
+        //valid user navigation to 'Create Account' page test
+        validNavigateToCreateAccountPageTest();
+        //valid male user account creation test
+        validMaleUserAccountCreationTest(createAccountPage);
+        //valid user address addition test
+        validUserAddressAdditionTest();
+        //return to homepage test
+        returnRegisteredUserToHomePage();
+        //navigate to 'Women' category dashboard page test (as a registered user)
+        navigateToWomenCategoryProductDashboardPageTest();
+        //add 'Blouse' and 'Printed Chiffon Dress' products to compare list test (as a registered user)
+        addBlouseAndPrintedDressToCompareListTest();
+        //remove 'Blouse' product to compare list test (as a registered user)
         removeBlouseFromCompareListTest();
     }
 
