a95ae6c8c9e69672b45be43c64d32e203fc77f68
RomanSibakovskij <j4023958@gmail.com>
2025-02-05 05:58:41 +0200

    Add empty SingleProductCategoryDashboardPage sheet

---
+++ src/main/java/com/nopcommerce/demo/SingleProductCategoryDashboardPage.java
@@ -0,0 +1,19 @@
+package com.nopcommerce.demo;
+
+import com.nopcommerce.demo.utilities.*;
+import org.openqa.selenium.*;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.*;
+import org.openqa.selenium.support.ui.*;
+
+import java.time.Duration;
+import java.util.*;
+
+public class SingleProductCategoryDashboardPage extends BasePage {
+
+    
+
+    public SingleProductCategoryDashboardPage(WebDriver driver) {super(driver);}
+
+
+}
