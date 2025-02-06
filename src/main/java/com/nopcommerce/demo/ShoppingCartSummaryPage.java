5d2c0e1f8923fef1dd08b47e07f8c2c037e87497
RomanSibakovskij <j4023958@gmail.com>
2025-02-06 09:33:59 +0200

    Add empty ShoppingCartSummaryPage sheet

---
+++ src/main/java/com/nopcommerce/demo/ShoppingCartSummaryPage.java
@@ -0,0 +1,16 @@
+package com.nopcommerce.demo;
+
+import com.nopcommerce.demo.utilities.BasePage;
+import org.openqa.selenium.*;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.*;
+
+import java.time.Duration;
+import java.util.*;
+
+public class ShoppingCartSummaryPage extends BasePage {
+
+    public ShoppingCartSummaryPage(WebDriver driver) {super(driver);}
+
+    
+}
