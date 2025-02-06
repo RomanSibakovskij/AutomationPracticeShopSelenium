b8cda23b79242d7c00565e431dcd780114da840e
RomanSibakovskij <j4023958@gmail.com>
2025-02-06 06:56:30 +0200

    Add Product Sort By Name (Z - A) Test (as a registered user) method

--- src/test/java/com/nopcommerce/demo/SingleProductCategoryDashboardPageTest.java
+++ src/test/java/com/nopcommerce/demo/SingleProductCategoryDashboardPageTest.java
@@ -144,6 +144,27 @@ public class SingleProductCategoryDashboardPageTest extends TestMethods{
         sortByNameZAFirstViewTest();
     }
 
+    //Test 007i -> product sort by product name (Z - A) first test (single category product dashboard page) (as a registered user)
+    @Test
+    @DisplayName("Product Sort By Name (Z - A) Test (as a registered user)")
+    @Tag("Product_Sort_By_View_Test")
+    @Tag("Test_As_A_Registered_User")
+    void regUserProductSortByNameZATest(){
+        CreateAccountPage createAccountPage = new CreateAccountPage(driver);
+        //valid user navigation to 'Create Account' page test
+        validNavigateToCreateAccountPageTest();
+        //valid male user account creation test
+        validMaleUserAccountCreationTest(createAccountPage);
+        //valid user address addition test
+        validUserAddressAdditionTest();
+        //return to homepage test
+        returnRegisteredUserToHomePage();
+        //navigate to 'Women' product category dashboard page test (as a registered user)
+        navigateToWomenCategoryProductDashboardPageTest();
+        //sort product by product name (Z - A) (as a registered user)
+        sortByNameZAFirstViewTest();
+    }
+
     //add products to compare list tests
 
     //Test 008 -> add 'Blouse' and 'Printed Chiffon Dress' products to compare list test (as a guest)
